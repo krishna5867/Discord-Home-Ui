0a3711f3885d145e7df7bd8d0e2a0dbcfec7543b
Krishna Kumar <106882782+krishna5867@users.noreply.github.com>
2023-02-19 22:08:14 +0530

    nav-open

--- script.js
+++
@@ -1,7 +0,0 @@
-const close = document.getElementsByClassName('toggle-close')
-
-let open = document.querySelector('.fa-bars');
-
-open.addEventListener("click", ()=>{
-    alert("Text Copied");
-})
